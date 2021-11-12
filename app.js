14cda1e26673b718b1d714c850a4d88fa2578d90
Carter Supple <Cartersupple123@gmail.com>
2021-11-11 23:24:24 -0800

    added statement for null answers

--- app.js
+++ app.js
@@ -12,21 +12,53 @@ const inputs = () => {
             type: 'input',
             name: 'github',
             message: 'Enter your GitHub username as it appears on your account.',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('A value is needed to continue.');
+                    return false; 
+                }
+            } 
         },
         {
             type: 'input',
             name:'email',
             message: 'Enter your email as it appears on your account.',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('A value is needed to continue.');
+                    return false; 
+                }
+            } 
         },
         {
             type:'input',
             name:'project name',
             message:'Enter a name for your project.',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('A value is needed to continue.');
+                    return false; 
+                }
+            } 
         },
         {
             type: 'input',
             name:'description',
             message:'Enter a description for your project.',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('A value is needed to continue.');
+                    return false; 
+                }
+            } 
         },
         {
             type: 'list',
@@ -40,6 +72,14 @@ const inputs = () => {
             'MIT',
             'Mozilla',
             'Open'],
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('A value is needed to continue.');
+                    return false; 
+                }
+            } 
         },
         {
             type: 'input',
@@ -50,16 +90,25 @@ const inputs = () => {
             type:'input',
             name:'usage',
             message:'Write purpose of application.',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('A value is needed to continue.');
+                    return false; 
+                }
+            } 
         },
         {
             type: 'input',
             name: 'contributors',
             message: 'Write name of contributors if any.',
-        }
+        },
         {
             type:'input',
             name:'tests',
             message:'Write steps of a test.',
         }
     ]);
-}
+};
+
