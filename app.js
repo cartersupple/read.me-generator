3c3937f0703c25154279e7a8c4d68af926500f03
Carter Supple <Cartersupple123@gmail.com>
2021-11-11 17:58:35 -0800

    require modules and link to generation page

---
+++ app.js
@@ -0,0 +1,8 @@
+// node modules
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// link to where the read.me page will generate
+const makeReadMe = require('makeReadMe.js')
+
+// make questions for inputs
